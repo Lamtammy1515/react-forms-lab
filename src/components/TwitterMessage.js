fedaea1a5421c76d2391931a7928e5015f7f040c
Tammy Lam <lamtammy1515@gmail.com>
2021-07-17 21:31:07 -0500

    Finished lab and passed all tests

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,15 +4,26 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message:""
+    };
+  }
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value
+    })
   }
 
+
   render() {
+    let charNum = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
-      </div>
+      
+        <input type="text" name="message" id="message" onChange = {this.handleChange} value={this.state.message}/>
+       {charNum} 
+       </div>
     );
   }
 }
